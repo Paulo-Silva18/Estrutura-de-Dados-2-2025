aaa8f85dc22f0d97695589f76a6fbb99700ef95f
Paulo Henrique Marques Silva <104027031+Paulo-Silva18@users.noreply.github.com>
2025-08-27 22:05:14 -0300

    Add files via upload

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,6 +1,6 @@
 def bubble_sort(lista):
   n = len(lista)
-  for i in range(n):
+  for i in range(n-1):
     for j in range(0, n-i-1):
       if lista[j] > lista[j+1]:
         lista[j], lista[j+1] = lista[j+1], lista[j]
